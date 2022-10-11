34e6252922601df415926b04b39231bd840d5ada
MJonas <mateusjonass23@gmail.com>
2022-10-10 21:04:29 -0300

    Update Cliente.java

--- src/main/java/br/iesp/edu/api/entity/Cliente.java
+++ src/main/java/br/iesp/edu/api/entity/Cliente.java
@@ -1,11 +1,13 @@
 package br.iesp.edu.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 
 @Getter
@@ -21,7 +23,8 @@ public class Cliente {
     private String cartao;
     private String cep;
     private String documento;
-    private String dataDENascimento;
+    @JsonFormat(pattern = "dd/mm/yy")
+    private LocalDate dataDENascimento;
     private String benefuicios ;
     private String assinatura;
 
